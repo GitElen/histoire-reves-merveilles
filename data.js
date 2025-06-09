a0a949c1cb2898484bef7cefbdc45d085828a751
GitElen <lessaveurs.elene@gmail.com>
2025-06-09 17:00:42 +1000

    Update data.js

--- data.js
+++ data.js
@@ -20,13 +20,13 @@ const homepagePacks = [
   }
 ];
 
-const genericDescription = (title) => {
-  Découvrez ${title}, un pack de coloriages conçu pour les enfants de 3 à 6 ans.
-  Il contient 10 illustrations originales et amusantes, parfaites pour stimuler la créativité et l’imagination.
-  Chaque dessin est prêt à être imprimé et colorié, idéal pour des moments calmes et joyeux à la maison.
-  
-  Téléchargez instantanément ce pack après votre achat et laissez la magie des couleurs illuminer la journée de votre enfant !
-}
+const genericDescription = (title) => `
+Découvrez ${title}, un pack de coloriages conçu pour les enfants de 3 à 6 ans.
+Il contient 10 illustrations originales et amusantes, parfaites pour stimuler la créativité et l’imagination.
+Chaque dessin est prêt à être imprimé et colorié, idéal pour des moments calmes et joyeux à la maison.
+
+Téléchargez instantanément ce pack après votre achat et laissez la magie des couleurs illuminer la journée de votre enfant !`;
+
 
 // Tous les produits pour les pages individuelles
 const products = [
